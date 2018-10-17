ae60264aa756a96ccb6387311bfd999295fc727f
imranfastnu <imran.khalid@emaratech.ae>
2018-10-17 16:33:19 +0400

    Added icons and fragment

--- app/src/main/java/com/goldenscent/android/MainActivity.java
+++ app/src/main/java/com/goldenscent/android/MainActivity.java
@@ -1,13 +1,24 @@
 package com.goldenscent.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvMenuOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvMenuOptions = (RecyclerView)findViewById(R.id.rvMenuOptions);
+
+        HomeFragment homeFragment = HomeFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, homeFragment)
+                .commit();
+
     }
 }

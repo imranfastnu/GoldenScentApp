b7e45ae2a8e9bce39733bf0318c54a4d4cf051cb
imranfastnu <imran.khalid@emaratech.ae>
2018-10-17 17:54:53 +0400

    Worked on Best seller list and banner

--- app/src/main/java/com/goldenscent/android/HomeFragment.java
+++ app/src/main/java/com/goldenscent/android/HomeFragment.java
@@ -2,12 +2,28 @@ package com.goldenscent.android;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.goldenscent.android.adapter.BestsellersAdapter;
+import com.goldenscent.android.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
+
+    private List<Product> productList;
+    private BestsellersAdapter adapterBestseller;
+
+    private TextView tvViewAll;
+    private RecyclerView rvBestSellers;
 
     public HomeFragment() {
 
@@ -27,7 +43,44 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        tvViewAll= view.findViewById(R.id.tvViewAll);
+        rvBestSellers  = view.findViewById(R.id.rvBestSellers);
+
+        setupBestsellersRecyclerView();
+
+        return view;
+    }
+
+    private void setupBestsellersRecyclerView() {
+        initializeProduct();
+        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.HORIZONTAL);
+        rvBestSellers.setLayoutManager(mLayoutManager);
+
+        rvBestSellers.setHasFixedSize(true);
+        rvBestSellers.setNestedScrollingEnabled(false);
+        //Drawable dividerDrawable = ContextCompat.getDrawable(getActivity(), R.drawable.divider_vertical);
+        //rvRecommendedProducts.addItemDecoration(new DividerItemDecoration(dividerDrawable));
+        rvBestSellers.setItemAnimator(new DefaultItemAnimator());
+        adapterBestseller = new BestsellersAdapter(getActivity(), productList);
+        rvBestSellers.setAdapter(adapterBestseller);
+    }
+
+
+    private void initializeProduct(){
+        productList = new ArrayList<>();
+        productList.add(new Product(R.mipmap.icon_bestseller1, "PRADA", "Candy", "348 SR", "121 SR"));
+        productList.add(new Product(R.mipmap.icon_bestseller2, "Dolge & Gabana sweet perfume", "The one text goes here untill the end for the layout", "348 SR", ""));
+        productList.add(new Product(R.mipmap.icon_bestseller3, "Aigner", "N. 1", "348 SR", ""));
+
+        productList.add(new Product(R.mipmap.icon_bestseller1, "PRADA", "Candy", "348 SR", "121 SR"));
+        productList.add(new Product(R.mipmap.icon_bestseller2, "Dolge & Gabana sweet perfume", "The one text goes here untill the end for the layout", "348 SR", ""));
+        productList.add(new Product(R.mipmap.icon_bestseller3, "Aigner", "N. 1", "348 SR", ""));
+
+        productList.add(new Product(R.mipmap.icon_bestseller1, "PRADA", "Candy", "348 SR", "121 SR"));
+        productList.add(new Product(R.mipmap.icon_bestseller2, "Dolge & Gabana sweet perfume", "The one text goes here untill the end for the layout", "348 SR", ""));
+        productList.add(new Product(R.mipmap.icon_bestseller3, "Aigner", "N. 1", "348 SR", ""));
     }
 
 }
